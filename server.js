5ab4e709afcaf689ab20396c1cfa653118ad0d49
sajid shaikh <sajidsultan312@gmail.com>
2025-07-14 01:21:16 +0530

    change in server file

--- server.js
+++ server.js
@@ -44,16 +44,9 @@ app.use(passport.session());
 
 // Connecting MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 10000 // 10 seconds
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
-
+  .catch((err) => console.error(err));
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
