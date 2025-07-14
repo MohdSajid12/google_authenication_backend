8cc509af161635e2e04f097aea60c09bc9675299
sajid shaikh <sajidsultan312@gmail.com>
2025-07-14 12:22:24 +0530

    small bug

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 const path = require("path");
 const multer = require('multer');
 
-require("./config/passport"); /
+require("./config/passport"); 
 
 const app = express();
 
